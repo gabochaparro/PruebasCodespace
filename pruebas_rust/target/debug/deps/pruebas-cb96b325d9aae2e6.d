ecd12c8b48f15c042259506ba6395cab38370b67
gabochaparro <143703358+gabochaparro@users.noreply.github.com>
2026-01-01 16:00:02 -0500

    Delete pruebas_rust/target directory

--- pruebas_rust/target/debug/deps/pruebas-cb96b325d9aae2e6.d
+++
@@ -1,5 +0,0 @@
-/workspaces/PruebasCodespace/pruebas_rust/target/debug/deps/pruebas-cb96b325d9aae2e6.d: src/main.rs
-
-/workspaces/PruebasCodespace/pruebas_rust/target/debug/deps/libpruebas-cb96b325d9aae2e6.rmeta: src/main.rs
-
-src/main.rs:
--- pruebas_rust/target/debug/pruebas.d
+++
@@ -1,1 +0,0 @@
-/workspaces/PruebasCodespace/pruebas_rust/target/debug/pruebas: /workspaces/PruebasCodespace/pruebas_rust/src/main.rs
